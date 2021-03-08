bf4085490088009b86ff3071ac6ffcde6cbd7794
NT. Hải <haithuongpro2711@gmail.com>
2021-03-08 10:00:53 +0000

    Update RDPcircleCI.ps1

--- RDPcircleCI.ps1
+++ RDPcircleCI.ps1
@@ -7,5 +7,5 @@ cmd /c echo ./ngrok.exe authtoken "$NGROK" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
  Write-Host "Here is your NGROK IP"
-curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
+cmd /c curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
 .\a.ps1
