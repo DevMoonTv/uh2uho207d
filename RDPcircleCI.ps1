2c5c80fde17932b1c4da388382e84be09b7097e9
NT. Hải <haithuongpro2711@gmail.com>
2021-03-08 09:44:40 +0000

    Update RDPcircleCI.ps1

--- RDPcircleCI.ps1
+++ RDPcircleCI.ps1
@@ -6,4 +6,5 @@ Copy ngrok.exe C:\Windows\System32
 cmd /c echo ./ngrok.exe authtoken "$NGROK" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
+cmd /c """tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel.Edit YML and try again " && taskkill /f /im powershell.exe"""
 .\a.ps1
