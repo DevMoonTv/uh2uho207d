f8fbcd08dba81b46cdb02949beecff9e794374f7
NT. Hải <haithuongpro2711@gmail.com>
2021-03-08 10:06:43 +0000

    Update RDPcircleCI.ps1

--- RDPcircleCI.ps1
+++ RDPcircleCI.ps1
@@ -6,6 +6,5 @@ Copy ngrok.exe C:\Windows\System32
 cmd /c echo ./ngrok.exe authtoken "$NGROK" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
- Write-Host "Here is your NGROK IP"
-cmd /c curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
+ Write-Host "Visit: https://dashboard.ngrok.com/endpoints/status to get NGROK IP"
 .\a.ps1
