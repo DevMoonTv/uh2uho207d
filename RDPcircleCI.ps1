45def907d68a547f22065a7aaf9e052f2befa8c1
NT. Hải <haithuongpro2711@gmail.com>
2021-03-08 09:56:22 +0000

    Update RDPcircleCI.ps1

--- RDPcircleCI.ps1
+++ RDPcircleCI.ps1
@@ -7,5 +7,5 @@ cmd /c echo ./ngrok.exe authtoken "$NGROK" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
 echo Your NGROK IP:
-cmd /c """tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || taskkill /f /im powershell.exe"""
+curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
 .\a.ps1
