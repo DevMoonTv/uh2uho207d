325692c9a759cfa3249996d50dbf6da52ea63c03
NT. Hải <haithuongpro2711@gmail.com>
2021-03-10 08:54:10 +0000

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -8,7 +8,7 @@ echo To change another VM region, Create New organization (Your current VM locat
 echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, pls update NGROK TOKEN in YML. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " && exit 
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, pls update NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit 
 echo User: Administrator
 echo Pass: Thuonghai001
 echo Subscribe: https://www.youtube.com/channel/UCi80Ipndm3QovIn5XisUz3g
